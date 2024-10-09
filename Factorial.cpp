e10be908770630a861e0574a5092ee0e659f8e9b
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-10-09 23:36:53 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,28 @@
+/ name: Kshitij Yadav
+//PRN: 23070123052
+//factorial using recusrion
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n<=1)
+        return 1;
+
+    else return (n*fact(n-1));
+}
+int main()
+{
+    int a;
+    cout<<"Enter an integer: "<<endl;
+    cin>>a;
+    cout<<"Factorial is: "<<fact(a);
+}
+
+/*
+OUTOUT: 
+Enter an integer: 
+5
+Factorial is: 120
+*/
